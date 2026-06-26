31bce215e57f701a5cac48d79f11e549bee4e450
ramaeb <bianucciramiro@hotmail.com>
2026-06-26 15:51:58 -0300

    new file:   src/publicaciones/entities/publicacion.entity.ts

---
+++ src/publicaciones/entities/publicacion.entity.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+@Schema({ timestamps: true }) // Genera automáticamente createdAt y updatedAt
+export class Publicacion extends Document {
+  @Prop({ required: true })
+  titulo!: string;
+
+  @Prop({ required: true })
+  descripcion!: string;
+
+  @Prop({ default: null })
+  imagenUrl!: string;
+
+  @Prop({ type: Types.ObjectId, ref: 'Usuario', required: true })
+  usuarioId!: Types.ObjectId;
+
+  @Prop({ required: true })
+  autorUsuario!: string;
+
+  @Prop({ default: true })
+  activo!: boolean; // Para la baja lógica
+
+  @Prop({ type: [Types.ObjectId], default: [] })
+  likes!: Types.ObjectId[]; // Almacena qué IDs de usuarios dieron like
+
+  @Prop({ type: [Object], default: [] })
+  comentarios!: any[];
+}
+
+export const PublicacionSchema = SchemaFactory.createForClass(Publicacion);
