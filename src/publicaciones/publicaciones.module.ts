01ae5cb0d5236cde2476a93e18ee9fa03fff375d
ramaeb <bianucciramiro@hotmail.com>
2026-06-26 17:49:16 -0300

    modified:   src/publicaciones/publicaciones.module.ts

--- src/publicaciones/publicaciones.module.ts
+++ src/publicaciones/publicaciones.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PublicacionesService } from './publicaciones.service';
 import { PublicacionesController } from './publicaciones.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
+import { Publicacion, PublicacionSchema } from './entities/publicacion.entity'; 
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { 
+        name: Publicacion.name, 
+        schema: PublicacionSchema   
+      }
+    ]),
+    CloudinaryModule, 
+  ],
   controllers: [PublicacionesController],
   providers: [PublicacionesService],
 })
