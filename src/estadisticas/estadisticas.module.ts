7b1cafbd2cf47728dd14a771a725e36fa6e5235e
ramaeb <bianucciramiro@hotmail.com>
2026-07-06 16:56:28 -0300

    modified:   src/estadisticas/estadisticas.module.ts

--- src/estadisticas/estadisticas.module.ts
+++ src/estadisticas/estadisticas.module.ts
@@ -4,11 +4,13 @@ import { EstadisticasController } from './estadisticas.controller';
 import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
 import { Publicacion, PublicacionSchema } from 'src/publicaciones/entities/publicacion.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import {UsuariosService} from "../usuarios/usuarios.service";
+import { UsuariosModule } from 'src/usuarios/usuarios.module';
 
 @Module({
   imports: [AuthModule,
     // 🚀 ¡CLAVE! Registramos el esquema para que EstadisticasService lo pueda inyectar
-    MongooseModule.forFeature([{ name: Publicacion.name, schema: PublicacionSchema }]),
+    MongooseModule.forFeature([{ name: Publicacion.name, schema: PublicacionSchema }]), UsuariosModule
   ],
   controllers: [EstadisticasController],
   providers: [EstadisticasService],
